6978bb6cfd5a69c30b88cc3a29c0798c8aad1f91
Kleer LA <kleer@kleer-sandbox.(none)>
2016-12-17 15:46:47 -0200

    CORREGIDO EL TDD

--- spec/devolver_palabra_secreta_spec.rb
+++ spec/devolver_palabra_secreta_spec.rb
@@ -1,8 +1,10 @@
 require 'ahorcado'
 describe Ahorcado do
 	it "Al gnar el juego deberia mostrar la palabra secreta cocoon" do
-		ahorcado = Ahorcado.new(6,"cocoon",6)
+		ahorcado = Ahorcado.new(6,"COCOON",6)
 		palabra_secreta = ahorcado.get_palabra_secreta()
-		palabra_secreta.should =="cocoon"
+		palabra_secreta.should =="COCOON"
 	end
+	
+
 end
